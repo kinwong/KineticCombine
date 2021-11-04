b7b39684123674621fa11f05036212f7227814d6
kinwong <kinhangwong@gmail.com>
2021-11-04 21:30:59 +1100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,6 @@
+# KineticCombine
+
+I followed this article to bootstrap the initial library.
+<https://dev.to/appwrite/swift-101-building-a-library-with-swift-package-manager-46h>
+
+Experimentation of combine operators.
---
+++ Tests/KineticCombineTests/KineticCombineTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import KineticCombine
+
+final class KineticCombineTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(KineticCombine().text, "Hello, World!")
+    }
+}
